d1409935b0fdd2c64edf3ee84dfe2cbcc9e62fcd
mkurolap <mkurolap@gmail.com>
2018-04-17 00:25:23 +0300

    Zeroes was added

--- script.js
+++ script.js
@@ -7,7 +7,27 @@ $(document).ready(function() {
 		var hour = time.getHours();
 		var minute = time.getMinutes();
 		var second = time.getSeconds();
-		var date = year + '.' + month + '.' + day + ' ' + hour + ':' + minute + ':' + second;
+				if (month > 9) {
+					month_0 = month;
+				} else 	          {
+					month_0 = '0' + month;
+				}
+					if (day > 9) {
+							day_0 = day;
+					} else 	          {
+							day_0 = '0' + day;
+					}
+						if (minute > 9) {
+							minute_0 = minute;
+						} else 	          {
+							minute_0 = '0' + minute;
+						}
+							if (second > 9) {
+								second_0 = second;
+							} else 	          {
+								second_0 = '0' + second;
+							}
+				var date = year + '.' + month_0 + '.' + day_0 + ' ' + hour + ':' + minute_0 + ':' + second_0;
 		$('#digital').text(date);
 	}, 1000);
 });
